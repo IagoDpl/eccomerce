798e03b9b2bebc7df6906b75d407653fceb35182
IagoDpl <iagodpl@hotmail.com>
2024-12-20 02:37:24 -0300

    navIcons

---
+++ src/components/CartModal.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+
+const CartModal = () => {
+  return (
+    <div>CartModel</div>
+  )
+}
+
+export default CartModal
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,38 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/navigation";
 
 const SearchBar = () => {
+  const router = useRouter();
+  const handleSearch = () => {
+    e.preventDefault();
+    const formData = new formData(e.currenTarget);
+    const name = formData.get("name");
 
-    const handleSearch = () =>{
-        e.preventDefault();
+    if (name) {
+      router.push(`/list?name=${name}`);
     }
+  };
 
   return (
-    <form className='flex ic justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1' onSubmit={handleSearch}>
-        <input type='text' placeholder='Search' className='flex-1 bg-transparent outline-none'/>
-        <button className='cursor-pointer'>
-            <FontAwesomeIcon icon={faMagnifyingGlass} width={16} height={16}/>
-        </button>
+    <form
+      className="flex ic justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1"
+      onSubmit={handleSearch}
+    >
+      <input
+        type="text"
+        name="name"
+        placeholder="Search"
+        className="flex-1 bg-transparent outline-none"
+      />
+      <button className="cursor-pointer">
+        <FontAwesomeIcon icon={faMagnifyingGlass} width={16} height={16} />
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
